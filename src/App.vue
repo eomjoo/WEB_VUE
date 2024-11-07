3f599c8add1385dfb58bd74fd83d5a8981afc9ea
jesuszero <djawndud5923@naver.com>
2024-11-08 00:57:09 +0900

    test

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>GitHub Pages에 Vue 프로젝트 배포 성공!</h1>
+    <p>버튼을 클릭하면 숫자가 증가테12321321321321스트 코드입니다 합니다:</p>
+    <button @click="incrementCounter">클릭: {{ counter }}</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    incrementCounter() {
+      this.counter++;
+    },
+  },
+};
 </script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 50px;
+}
+button {
+  font-size: 16px;
+  padding: 10px 20px;
+  margin-top: 20px;
+  cursor: pointer;
 }
 </style>
